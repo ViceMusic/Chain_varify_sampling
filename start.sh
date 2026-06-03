790f61355af297176e48129a865e713dc75400ce
root <xarnudvilas@gmail.com>
2026-06-03 10:54:59 +0800

    new

--- start.sh
+++ start.sh
@@ -1,3 +1,1 @@
-nohup python -u start_random_selection.py > ./logs/start_random_selection_ppc1.txt 2>&1 &
-nohup python -u start_fps_selection.py > ./logs/start_fps_selection_ppc1.txt 2>&1 &
-nohup python -u start_kcenter_selection.py > ./logs/start_kcenter_selection_ppc1.txt 2>&1 &
+nohup python -u AdaSADM_FULL.py > ./logs/AdaSADM_FULL_PPC=10.txt 2>&1 &
